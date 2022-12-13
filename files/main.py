1c8ffeb43f4c4fafc58a02a3e7ffbe140b53c136
Sasha-v-tanke <s4sha-v-tanke@yandex.ru>
2022-12-13 21:06:00 +0300

    набросал структуру проекта

---
+++ files/main.py
@@ -0,0 +1,16 @@
+def checkImports() -> bool:
+    return True
+
+
+class Manager:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    if checkImports():
+        manager = Manager()
+        manager.run()
